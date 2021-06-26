986b70c03d188bb9d3785a547605f2b043061653
Adam Holder <adam.holder.92@gmail.com>
2021-06-26 12:16:02 -0400

    Updating comments in Problem 5.py

--- ProjectEuler/Program 5.py
+++ ProjectEuler/Program 5.py
@@ -5,12 +5,13 @@
 import math
 
 def FactorsUpTo (target: int) -> list:
-    #create a list of factors up to a "target"
+    #create a list of factors [1,2.....,target]
     FactorList = []
     for i in range(1,target+1):
         FactorList.append(i)
     return FactorList
 
+#creates a list of factors [1,2,.....,target] and saves it to FactorList
 target = int(input("Input the highest factor: "))
 FactorList = FactorsUpTo(target)
 
@@ -18,7 +19,9 @@ solution = 0
 ApprovedFactors = 0
 
 for i in range(1,1000000000):
+    #iterate through every i up to a range, currently 1000000000
     for factor in FactorList:
+        #iterate through each factor in
         if i % factor == 0:
             ApprovedFactors += 1
         if i % factor != 0:
