5b62db8f0f64e98720be3289890203f904e3f367
Adam Holder <adam.holder.92@gmail.com>
2021-06-27 06:41:06 -0400

    More comments for Program 5.py

--- ProjectEuler/Program 5.py
+++ ProjectEuler/Program 5.py
@@ -21,14 +21,18 @@ ApprovedFactors = 0
 for i in range(1,1000000000):
     #iterate through every i up to a range, currently 1000000000
     for factor in FactorList:
-        #iterate through each factor in
+        #iterate through each factor in FactorList and check:
+        #if i is divisible by factor, if so, ApprovedFactors ticks up by 1.
         if i % factor == 0:
             ApprovedFactors += 1
+        #if i is NOT divisible by factor, set ApprovedFactors back to 0 and break to the next i.
         if i % factor != 0:
             ApprovedFactors = 0
             break
+        #if we make it to the end of the FactorList and everything is divisible, we found the lowest solution. Set solution to the value of i.
         if ApprovedFactors == target:
             solution = i
+    #when i is NOT 0, we have found the solution. Break out of the code and finish.
     if solution != 0:
         break
 print(solution)
