c53d3ef382cec434172fd5b7a1e51fb587e81ff7
adam-holder <adam.holder.92@gmail.com>
2023-02-09 15:13:42 -0500

    Fixed Formatting

--- ProjectEuler/Problem 10.py
+++ ProjectEuler/Problem 10.py
@@ -2,18 +2,18 @@
 # Find the sum of all the primes below two million.
 import math
 def isPrime(n)-> bool:
-    if n<=1:
+    if n <= 1:
         return False
-    if n<=3:
+    if n <= 3:
         return True
-    if (n%2 ==0 or n%3 ==0):
+    if (n % 2 == 0 or n % 3 == 0):
         return False
-    i=5 #Lowest positive 6k-1 value
+    i = 5 #Lowest positive 6k-1 value
     while(math.pow(i,2)<=n):
         #if n is evenly divisble by 6k-1 or 6k+1, it is not prime.
-        if(n%i==0 or n%(i+2)==0):
+        if(n % i == 0 or n % (i + 2) == 0):
             return False
-        i+=6
+        i += 6
     return True
 
 primeList = []
